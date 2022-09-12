752490b1922be710f099b703cd641c287ce4a2b1
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-09-12 05:43:09 +0000

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,27 @@
-import express,{ Application,Request,Response,NextFunction} from 'express'
-import bodyParser from 'body-parser'
+import express,{ Application,Request,Response,NextFunction } from 'express';
+import "dotenv/config";
+import bodyParser from 'body-parser';
+import Routes from './Routes';
+import Connect from './connect';
 
 const app : Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req:Request, res : Response ) => {
+
+app.get('/',(req:Request,res : Response ) => {
 
     res.send('TS App is Running')
 })
 
 const PORT = process.env.PORT;
+const db = 'mongodb://localhost:27017/test';
 
-app.listen(PORT, () => {
+
+Connect({ db });
+Routes({ app })
+
+app.listen(PORT,() => {
     console.log(`server is running on PORT ${PORT}`)
 })
